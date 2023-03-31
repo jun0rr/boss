868c0b3ae289972fb9f6a000e10f670ee073927b
Juno Roesler <juno.rr@gmail.com>
2023-03-31 15:16:04 -0300

    Refactored com.jun0rr.boss.volume.Cached to record.

--- src/main/java/com/jun0rr/boss/volume/Cached.java
+++ src/main/java/com/jun0rr/boss/volume/Cached.java
@@ -5,83 +5,26 @@
 package com.jun0rr.boss.volume;
 
 import java.time.Instant;
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
  * @author F6036477
  */
-public class Cached<T> implements Comparable<Cached> {
+public record Cached<T>(T content, AtomicInteger count, Instant time) implements Comparable<Cached> {
   
-  private final T content;
-  
-  private final AtomicInteger count;
-  
-  private final Instant time;
-  
-  public Cached(T cont) {
-    this.content = Objects.requireNonNull(cont);
-    this.count = new AtomicInteger(0);
-    this.time = Instant.now();
-  }
-  
-  public static <U> Cached<U> of(U cont) {
-    return new Cached(cont);
-  }
-  
-  public T content() {
-    count.incrementAndGet();
-    return content;
-  }
-  
-  public int count() {
-    return count.get();
-  }
-  
-  public Instant time() {
-    return time;
+  public Cached(T content) {
+    this(content, new AtomicInteger(0), Instant.now());
   }
   
   @Override
   public int compareTo(Cached o) {
-    int c = Integer.compare(count(), o.count());
+    int c = Integer.compare(count().get(), o.count().get());
     return c != 0 ? c : time().compareTo(o.time());
   }
 
-  @Override
-  public int hashCode() {
-    int hash = 7;
-    hash = 83 * hash + Objects.hashCode(this.content);
-    hash = 83 * hash + Objects.hashCode(this.count);
-    hash = 83 * hash + Objects.hashCode(this.time);
-    return hash;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    final Cached<?> other = (Cached<?>) obj;
-    if (!Objects.equals(this.content, other.content)) {
-      return false;
-    }
-    if (!Objects.equals(this.count, other.count)) {
-      return false;
-    }
-    return Objects.equals(this.time, other.time);
-  }
-
-  @Override
-  public String toString() {
-    return "Cached{" + "content=" + content + ", count=" + count + ", time=" + time + '}';
+  public static <U> Cached<U> of(U cont) {
+    return new Cached(cont);
   }
   
 }
