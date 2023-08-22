9782145c42938c8e1f8e5cd5688d4152c9445133
Juno Roesler <juno.rr@gmail.com>
2023-08-22 16:07:27 -0300

    Implementing...

--- src/main/java/com/jun0rr/boss/config/BossConfig.java
+++ src/main/java/com/jun0rr/boss/config/BossConfig.java
@@ -6,9 +6,18 @@ package com.jun0rr.boss.config;
 
 import com.esotericsoftware.yamlbeans.YamlException;
 import com.esotericsoftware.yamlbeans.YamlReader;
+import com.jun0rr.binj.BinCodec;
+import com.jun0rr.binj.BinType;
+import com.jun0rr.binj.mapping.ConstructFunction;
+import com.jun0rr.binj.mapping.ExtractFunction;
+import com.jun0rr.binj.mapping.InjectFunction;
+import com.jun0rr.binj.mapping.InvokeStrategy;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  *
@@ -37,6 +46,184 @@ public record BossConfig(VolumeConfig volume, MapperConfig mapper) {
     catch(YamlException e) {
       throw new BossConfigException(e);
     }
+  }
+  
+  public static Builder builder() {
+    return new Builder();
+  }
+  
+  
+  
+  public static class Builder {
+    
+    private BufferConfig.Type bufferType;
+    
+    private int bufferSize;
+    
+    private long maxCacheSize;
+    
+    private final Map<Class,BinCodec> codecs;
+    
+    private InvokeStrategy<ConstructFunction> mappingConstructStrategy;
+    
+    private InvokeStrategy<ExtractFunction> mappingExtractStrategy;
+    
+    private InvokeStrategy<InjectFunction> mappingInjectStrategy;
+    
+    private String volumeId;
+    
+    private Path volumeStorePath;
+    
+    public Builder() {
+      this.codecs = new HashMap<>();
+      this.bufferType = BufferConfig.Type.HEAP;
+    }
+    
+    public Builder addCodec(BinCodec codec) {
+      codecs.put(Objects.requireNonNull(codec).bintype().type(), codec);
+      return this;
+    }
+    
+    public Map<Class,BinCodec> getCodecs() {
+      return codecs;
+    }
+
+    public BufferConfig.Type getBufferType() {
+      return bufferType;
+    }
+
+    public Builder setBufferType(BufferConfig.Type bufferType) {
+      this.bufferType = bufferType;
+      return this;
+    }
+
+    public int getBufferSize() {
+      return bufferSize;
+    }
+
+    public Builder setBufferSize(int bufferSize) {
+      this.bufferSize = bufferSize;
+      return this;
+    }
+
+    public long getMaxCacheSize() {
+      return maxCacheSize;
+    }
+
+    public Builder setMaxCacheSize(long maxCacheSize) {
+      this.maxCacheSize = maxCacheSize;
+      return this;
+    }
+
+    public InvokeStrategy<ConstructFunction> getMappingConstructStrategy() {
+      return mappingConstructStrategy;
+    }
+
+    public Builder setMappingConstructStrategy(InvokeStrategy<ConstructFunction> mappingConstructStrategy) {
+      this.mappingConstructStrategy = mappingConstructStrategy;
+      return this;
+    }
+
+    public InvokeStrategy<ExtractFunction> getMappingExtractStrategy() {
+      return mappingExtractStrategy;
+    }
+
+    public Builder setMappingExtractStrategy(InvokeStrategy<ExtractFunction> mappingExtractStrategy) {
+      this.mappingExtractStrategy = mappingExtractStrategy;
+      return this;
+    }
+
+    public InvokeStrategy<InjectFunction> getMappingInjectStrategy() {
+      return mappingInjectStrategy;
+    }
+
+    public Builder setMappingInjectStrategy(InvokeStrategy<InjectFunction> mappingInjectStrategy) {
+      this.mappingInjectStrategy = mappingInjectStrategy;
+      return this;
+    }
+
+    public String getVolumeId() {
+      return volumeId;
+    }
+
+    public Builder setVolumeId(String volumeId) {
+      this.volumeId = volumeId;
+      return this;
+    }
+
+    public Path getVolumeStorePath() {
+      return volumeStorePath;
+    }
+
+    public Builder setVolumeStorePath(Path volumeStorePath) {
+      this.volumeStorePath = volumeStorePath;
+      return this;
+    }
+    
+    public BossConfig build() {
+      BufferConfig bc = new BufferConfig(bufferType, bufferSize, maxCacheSize);
+      
+    }
+
+    @Override
+    public int hashCode() {
+      int hash = 5;
+      hash = 31 * hash + Objects.hashCode(this.bufferType);
+      hash = 31 * hash + this.bufferSize;
+      hash = 31 * hash + (int) (this.maxCacheSize ^ (this.maxCacheSize >>> 32));
+      hash = 31 * hash + Objects.hashCode(this.codecs);
+      hash = 31 * hash + Objects.hashCode(this.mappingConstructStrategy);
+      hash = 31 * hash + Objects.hashCode(this.mappingExtractStrategy);
+      hash = 31 * hash + Objects.hashCode(this.mappingInjectStrategy);
+      hash = 31 * hash + Objects.hashCode(this.volumeId);
+      hash = 31 * hash + Objects.hashCode(this.volumeStorePath);
+      return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (obj == null) {
+        return false;
+      }
+      if (getClass() != obj.getClass()) {
+        return false;
+      }
+      final Builder other = (Builder) obj;
+      if (this.bufferSize != other.bufferSize) {
+        return false;
+      }
+      if (this.maxCacheSize != other.maxCacheSize) {
+        return false;
+      }
+      if (!Objects.equals(this.volumeId, other.volumeId)) {
+        return false;
+      }
+      if (this.bufferType != other.bufferType) {
+        return false;
+      }
+      if (!Objects.equals(this.codecs, other.codecs)) {
+        return false;
+      }
+      if (!Objects.equals(this.mappingConstructStrategy, other.mappingConstructStrategy)) {
+        return false;
+      }
+      if (!Objects.equals(this.mappingExtractStrategy, other.mappingExtractStrategy)) {
+        return false;
+      }
+      if (!Objects.equals(this.mappingInjectStrategy, other.mappingInjectStrategy)) {
+        return false;
+      }
+      return Objects.equals(this.volumeStorePath, other.volumeStorePath);
+    }
+
+    @Override
+    public String toString() {
+      return "BossConfig.Builder{" + "bufferType=" + bufferType + ", bufferSize=" + bufferSize + ", maxCacheSize=" + maxCacheSize + ", codecs=" + codecs + ", mappingConstructStrategy=" + mappingConstructStrategy + ", mappingExtractStrategy=" + mappingExtractStrategy + ", mappingInjectStrategy=" + mappingInjectStrategy + ", volumeId=" + volumeId + ", volumeStorePath=" + volumeStorePath + '}';
+    }
+    
   }
   
 }
--- src/test/java/com/jun0rr/boss/test/TestObjectStore.java
+++ src/test/java/com/jun0rr/boss/test/TestObjectStore.java
@@ -4,8 +4,22 @@
  */
 package com.jun0rr.boss.test;
 
+import com.jun0rr.binj.mapping.AnnotationConstructStrategy;
+import com.jun0rr.binj.mapping.AnnotationExtractStrategy;
+import com.jun0rr.binj.mapping.AnnotationInjectStrategy;
+import com.jun0rr.binj.mapping.CombinedStrategy;
+import com.jun0rr.binj.mapping.ConstructFunction;
+import com.jun0rr.binj.mapping.DefaultConstructStrategy;
+import com.jun0rr.binj.mapping.ExtractFunction;
+import com.jun0rr.binj.mapping.FieldGetterStrategy;
+import com.jun0rr.binj.mapping.FieldSetterStrategy;
+import com.jun0rr.binj.mapping.FieldsOrderConstructStrategy;
+import com.jun0rr.binj.mapping.GetterStrategy;
+import com.jun0rr.binj.mapping.InjectFunction;
+import com.jun0rr.binj.mapping.SetterStrategy;
 import com.jun0rr.boss.ObjectStore;
 import com.jun0rr.boss.config.BossConfig;
+import com.jun0rr.boss.config.BufferConfig;
 import com.jun0rr.boss.store.DefaultObjectStore;
 import com.jun0rr.boss.test.record.Address;
 import com.jun0rr.boss.test.record.Person;
@@ -24,11 +38,61 @@ import org.junit.jupiter.api.Test;
 public class TestObjectStore {
   
   @Test
-  public void test() {
+  public void testFileStore() {
+    System.out.printf("---> testFileStore() <---");
     try {
       Path path = Paths.get("./TestObjectStore.bin");
       Uncheck.call(()->Files.deleteIfExists(path));
       BossConfig config = BossConfig.from(TestObjectStore.class.getResourceAsStream("/boss.yml"));
+      ObjectStore store = new DefaultObjectStore(config);
+      System.out.println(store);
+      for(int i = 0; i < 10; i++) {
+        Person p = new Person("John-" + i, "Doe-" +i, LocalDate.now(), new Address("Street-" + (i + 1), "City-" + (i + 1), i + 100));
+        System.out.println(store.store(p));
+      }
+      store.createIndex(Person.class, "name", Person::name);
+      store.createIndex(Person.class, "address.number", p->p.address().number());
+      store.find(Person.class, p->p.name().equals("John-5")).forEach(System.out::println);
+      store.close();
+      
+      store = new DefaultObjectStore(config);
+      for(int i = 0; i < 10; i++) {
+        Person p = new Person("John-" + i, "Doe-" +i, LocalDate.now(), new Address("Street-" + (i + 1), "City-" + (i + 1), i + 100));
+        Assertions.assertEquals(p, store.find(Person.class, "name", p.name()).findFirst().get().object());
+      }
+      store.close();
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+      throw e;
+    }
+  }
+  
+  @Test
+  public void testMemStore() {
+    System.out.printf("---> testMemStore() <---");
+    try {
+      Path path = Paths.get("./TestObjectStore.bin");
+      Uncheck.call(()->Files.deleteIfExists(path));
+      BossConfig config = BossConfig.from(TestObjectStore.class.getResourceAsStream("/boss.yml"));
+      BossConfig config = BossConfig.builder()
+          .setBufferSize(1024)
+          .setBufferType(BufferConfig.Type.DIRECT)
+          .setMappingConstructStrategy(new CombinedStrategy<ConstructFunction>()
+              .put(0, new FieldsOrderConstructStrategy())
+              .put(1, new DefaultConstructStrategy())
+              .put(2, new AnnotationConstructStrategy()))
+          .setMappingExtractStrategy(new CombinedStrategy<ExtractFunction>()
+              .put(0, new GetterStrategy())
+              .put(1, new FieldGetterStrategy())
+              .put(2, new AnnotationExtractStrategy()))
+          .setMappingInjectStrategy(new CombinedStrategy<InjectFunction>()
+              .put(0, new SetterStrategy())
+              .put(1, new FieldSetterStrategy())
+              .put(2, new AnnotationInjectStrategy()))
+          .setMaxCacheSize(4*1024*1024)
+          .setVolumeId("memVolume")
+          ;
       ObjectStore store = new DefaultObjectStore(config);
       System.out.println(store);
       for(int i = 0; i < 10; i++) {
